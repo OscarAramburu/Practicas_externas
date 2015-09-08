65b5ff98f44a99d3a1689f78e1bfe7e8522d0917
Oscar Aramburu <oscararamburugonzalez@gmail.com>
2015-09-08 12:54:56 +0200

    Eliminar lineas de comentarios

--- gffmap.py
+++ gffmap.py
@@ -3,7 +3,13 @@ grch37= open('GRCh37-GRCh38.gff', 'r')
 identificadores=[]
 
 for line in grch37:
-    print(line.split(sep='\t')[0])
+    if line.startswith('#'):
+        pass
+    else:
+        identificadores.append(line.split(sep='\t')[0])
+
+
+print(identificadores[:5])
     
 grch37.close()
 
