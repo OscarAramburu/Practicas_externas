89a0813001c9d8d1970e1345d40b5f110950a763
Oscar Aramburu <oscararamburugonzalez@gmail.com>
2015-09-08 13:09:57 +0200

    Eliminamos identificadores repetidos.

--- gffmap.py
+++ gffmap.py
@@ -2,14 +2,22 @@ grch37= open('GRCh37-GRCh38.gff', 'r')
 
 identificadores=[]
 
+
+
 for line in grch37:
     if line.startswith('#'):
         pass
     else:
-        identificadores.append(line.split(sep='\t')[0])
+    
+        ident=line.split(sep='\t')[0]
+        if ident in identificadores:
+            pass
+        else:
+            identificadores.append(ident)
+        
+        
 
-
-print(identificadores[:5])
+print(identificadores[:20])
     
 grch37.close()
 
