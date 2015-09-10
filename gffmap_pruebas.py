d3c5d4ca9275bfe97061b501f6beeb89d66d2798
Oscar Aramburu <oscararamburugonzalez@gmail.com>
2015-09-10 10:09:34 +0200

    Refactorización de codigo

---
+++ gffmap_pruebas.py
@@ -0,0 +1,23 @@
+grch37= open('GRCh37-GRCh38.gff', 'r')
+
+identificadores=[]
+
+
+
+for line in grch37:
+    
+    if not line.startswith('#'):
+        
+        ident=line.split(sep='\t')[0]
+        
+        if ident not in identificadores:
+            
+            identificadores.append(ident)
+  
+        
+print(identificadores[:20])
+    
+grch37.close()
+
+
+
