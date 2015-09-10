7f565493a59f18758476653030d1df18ec53e5f2
Xabi Bello <xbello@gmail.com>
2015-09-10 12:36:04 +0200

    PEP8

--- gffmap_pruebas.py
+++ gffmap_pruebas.py
@@ -1,23 +1,19 @@
+"""Esto hace tal cosa."""
 grch37= open('GRCh37-GRCh38.gff', 'r')
 
 identificadores=[]
 
-
-
 for line in grch37:
-    
+
     if not line.startswith('#'):
-        
-        ident=line.split(sep='\t')[0]
-        
+
+        ident = line.split(sep='\t')[0]
+
         if ident not in identificadores:
-            
+
             identificadores.append(ident)
-  
-        
+
+
 print(identificadores[:20])
-    
+
 grch37.close()
-
-
-
