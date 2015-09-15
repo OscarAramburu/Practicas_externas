dfbffe5348597b8d1c5a2a58a459c1826369be18
Oscar Aramburu <oscararamburugonzalez@gmail.com>
2015-09-15 10:57:27 +0200

    test de remap.

--- test_gffmap.py
+++ test_gffmap.py
@@ -1,6 +1,8 @@
 """Tests para gffmap."""
 from gffmap import parse_line
 
+from gffmap import remap
+
 
 def test_parse_line():
     """Test the GFF parser."""
@@ -19,3 +21,19 @@ def test_parse_line():
                     'inicio': 68285,
                     'fin': 75572,
                     'direccion': '+'}}
+
+def test_remap():
+
+    mapeo = {
+        'NT_187509.1':[{
+            'ident': "NT_187509.1",
+            'inicio': 3463,
+            'fin': 10794,
+            'direccion': "+",
+            'gap': 'M62 D47 M3126 I4 M725 D1 M3371',
+            'destino': {'identificador': 'NT_113885.1',
+                        'inicio': 68285,
+                        'fin': 75572,
+                        'direccion': '+'}}]}
+
+    assert remap('NT_187509.1', 4000, mapeo) == ('NT_113885.1', 3953)
