19e37962ef6b66fee3af22517fc600972f272cc3
Oscar Aramburu <oscararamburugonzalez@gmail.com>
2015-09-17 12:58:02 +0200

    Acceso a NCBI y obtencion de chromosome. Test aun falla

---
+++ NCBI_chr.py
@@ -0,0 +1,15 @@
+from Bio import Entrez
+from Bio.SeqIO.InsdcIO import GenBankIterator
+
+
+Entrez.email = 'oscararamburugonzale@gmail.com'
+
+
+def genbankid_to_chromosome(ident):
+    handle = Entrez.efetch(db='nucleotide',id=ident,rettype='gb',retmode='text')
+    #list_of_features = [SR.features for SR in GenBankIterator(handle)]
+    for records in GenBankIterator(handle):
+        chromosome = records.features[0].qualifiers['chromosome'][0]
+
+
+    return chromosome
