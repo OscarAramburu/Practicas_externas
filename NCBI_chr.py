57fa6f5310855cd4702dd1018887ba48ae24f14e
Oscar Aramburu <oscararamburugonzalez@gmail.com>
2015-09-18 11:05:31 +0200

    Definida variable posición. Pasan los 3 test.

--- NCBI_chr.py
+++ NCBI_chr.py
@@ -10,5 +10,9 @@ def genbankid_to_chromosome(ident):
     #list_of_features = [SR.features for SR in GenBankIterator(handle)]
     for records in GenBankIterator(handle):
         chromosome = records.features[0].qualifiers['chromosome'][0]
+        if 'map' in records.features[0].qualifiers:
+            position1 = records.features[0].qualifiers['map'][0]
+            position = position1.replace('unlocalized',chromosome+'-Unknown')
+            return position
 
     return chromosome
