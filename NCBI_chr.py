4b1ccefc11617b63302bfa2d31e1e58933a4e6b2
Oscar Aramburu <oscararamburugonzalez@gmail.com>
2015-09-18 11:09:07 +0200

    Refactorización

--- NCBI_chr.py
+++ NCBI_chr.py
@@ -9,9 +9,10 @@ def genbankid_to_chromosome(ident):
     handle = Entrez.efetch(db='nucleotide',id=ident,rettype='gb',retmode='text')
     #list_of_features = [SR.features for SR in GenBankIterator(handle)]
     for records in GenBankIterator(handle):
-        chromosome = records.features[0].qualifiers['chromosome'][0]
-        if 'map' in records.features[0].qualifiers:
-            position1 = records.features[0].qualifiers['map'][0]
+        qualifiers = records.features[0].qualifiers
+        chromosome = qualifiers['chromosome'][0]
+        if 'map' in qualifiers:
+            position1 = qualifiers['map'][0]
             position = position1.replace('unlocalized',chromosome+'-Unknown')
             return position
 
