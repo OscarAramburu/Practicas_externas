e11ed875887aa30b03fef5194f60eafc36e12d19
Xabi Bello <xbello@gmail.com>
2015-09-18 09:37:34 +0200

    Mocked the call to NCBI to save test time.

--- NCBI_chr.py
+++ NCBI_chr.py
@@ -6,10 +6,12 @@ Entrez.email = 'oscararamburugonzale@gmail.com'
 
 
 def genbankid_to_chromosome(ident):
-    handle = Entrez.efetch(db='nucleotide',id=ident,rettype='gb',retmode='text')
+    handle = Entrez.efetch(db='nucleotide',
+                           id=ident,
+                           rettype='gb',
+                           retmode='text')
     #list_of_features = [SR.features for SR in GenBankIterator(handle)]
     for records in GenBankIterator(handle):
         chromosome = records.features[0].qualifiers['chromosome'][0]
-
 
     return chromosome
--- test_NCBI_chr.py
+++ test_NCBI_chr.py
@@ -1,8 +1,18 @@
+import os
+from unittest.mock import patch
+
 from NCBI_chr import genbankid_to_chromosome
 
 
-def test_genbankid_to_chromosome():
+def efetch_from_disk(id=None, db=None, retmode=None, rettype=None):
+    if os.path.isfile(os.path.join("test_files", id + ".gb")):
+        handle = open(os.path.join("test_files", id + ".gb"))
+        return handle
+
+
+@patch("Bio.Entrez.efetch", side_effect=efetch_from_disk)
+def test_genbankid_to_chromosome(efetch_mock):
 
     assert genbankid_to_chromosome('NC_000001.11') == '1'
-    assert genbankid_to_chromosome('NC_113888.1') == '14-Unknown'
-    assert genbankid_to_chromosome('NC_187509.1') == 'Unknown'
+    assert genbankid_to_chromosome('NT_113888.1') == '14-Unknown'
+    assert genbankid_to_chromosome('NT_187509.1') == 'Unknown'
